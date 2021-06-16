faeee6be391e8b2d5bf436886f490c723633e098
shil-ydv <39042648+shil-ydv@users.noreply.github.com>
2021-06-16 16:27:18 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
 public class HelloWorld {
      public static void main(String[] args)
-     { System.out.println("Hello Wold and Welcome Jenkins Training New -chnge 1 ***");}
+     { System.out.println("Hello Wold and Welcome Jenkins Training New -chnge 2 ***");}
 }
